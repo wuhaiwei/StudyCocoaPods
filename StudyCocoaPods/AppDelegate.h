19cb9ceeb80b691d07d1a80e87976e158aee3598
wuhaiwei <wuhaiwei@qding.me>
2018-08-20 11:37:41 +0800

    Initial Commit

---
+++ StudyCocoaPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StudyCocoaPods
+//
+//  Created by hu on 2018/8/20.
+//  Copyright © 2018年 wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StudyCocoaPods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StudyCocoaPods
+//
+//  Created by hu on 2018/8/20.
+//  Copyright © 2018年 wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
