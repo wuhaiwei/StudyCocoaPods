397c4343a001c3f6c50f43ac14c4654515cdfa70
wuhaiwei <wuhaiwei@qding.me>
2018-08-20 11:41:32 +0800

    man

---
+++ StudyCocoaPods/Man.h
@@ -0,0 +1,15 @@
+//
+//  Man.h
+//  StudyCocoaPods
+//
+//  Created by hu on 2018/8/20.
+//  Copyright © 2018年 wu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Man : NSObject
+
+- (void)marry;
+
+@end
---
+++ StudyCocoaPods/Man.m
@@ -0,0 +1,16 @@
+//
+//  Man.m
+//  StudyCocoaPods
+//
+//  Created by hu on 2018/8/20.
+//  Copyright © 2018年 wu. All rights reserved.
+//
+
+#import "Man.h"
+
+@implementation Man
+- (void)marry
+{
+    NSLog(@"我结婚啦!");
+}
+@end
